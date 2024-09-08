ff998426f1adbf79507bb78d25cc7a0ff5521419
surya s <surya@mplgaming.com>
2024-09-08 22:32:20 +0530

    update

--- app/src/components/MetricPanel.tsx
+++ app/src/components/MetricPanel.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { useRef } from "react";
-import { useDrag } from "react-dnd";
+import React from "react";
 import { useCampaign } from "../context/CampaignContext";
 import Metric from "./Metric";
 
